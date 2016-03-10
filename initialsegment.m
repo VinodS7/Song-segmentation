b1a08e2760c749eb91bdca912f72b2a327f5a9fd
VinodS7 <student@lawn-143-215-91-175.lawn.gatech.edu>
2016-03-10 12:13:54 -0500

    Autocross and novelty analysis
    Removed ‘coffee’ and added subplot for novelty function

--- initialsegment.m
+++ initialsegment.m
@@ -27,9 +27,9 @@ for k = 1:i
     X = abs(X);
     [~, HD, ~, ~, ~] = PfNmf(X, param.WD, [], [], [], param.rh, param.sparsity);
     
-        [r1,~] = xcorr(HD(1,:),'coeff');
-        [r2,~] = xcorr(HD(2,:),'coeff');
-        [r3,~] = xcorr(HD(3,:),'coeff');
+        [r1,~] = xcorr(HD(1,:));
+        [r2,~] = xcorr(HD(2,:));
+        [r3,~] = xcorr(HD(3,:));
         [pks1,loc1] = findpeaks(r1(length(HD(1,:))+35:length(HD(1,:))+104));
         [pks2,loc2] = findpeaks(r2(length(HD(2,:))+35:length(HD(2,:))+104));
         [pks3,loc3] = findpeaks(r3(length(HD(3,:))+35:length(HD(3,:))+104));
@@ -43,27 +43,42 @@ for k = 1:i
         [~,I3] = max(pks3);
         period_samples3(k)= (loc3(I3)+34)*512;
         
-         [r,~] = xcorr((HD(1,:)+HD(2,:)+HD(3,:)),'coeff');
+         [r,~] = xcorr((HD(1,:)+HD(2,:)+HD(3,:)));
         [pks,loc] = findpeaks(r(length(HD(1,:))+35:length(HD(1,:))+104));
         [~,I] = max(pks);
         period_samples(k)= (loc(I)+34)*512;
         
         figure(1)
+        subplot(211)
         plot(r1)
         title('Autocorrelation for Hi-hat')
+        subplot(212)
+        plot(HD(1,:))
+        title('Novelty function Hi-Hat')
         
         figure(2)
+        subplot(211)
         plot(r2)
         title('Autocorrelation for Bass drum')
+        subplot(212)
+        plot(HD(2,:))
+        title('Novelty function Bass Drum')
         
         figure(3)
+        subplot(211)
         plot(r3)
         title('Autocorrelation for Snare drum')
+        subplot(212)
+        plot(HD(3,:))
+        title('Novelty function Snare Drum')
         
         figure(4)
+        subplot(211)
         plot(r)
         title('Autocorrelation for combined')
-    
+        subplot(212)
+        plot((HD(1,:)+HD(2,:)+HD(3,:)))
+        title('Novelty function Combined')
         
 end
     
